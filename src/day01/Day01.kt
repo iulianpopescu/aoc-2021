a8ec9bfd73f8569ea0d8e1a53fd54254e4b6c3cc
Iulian Popescu <iulianpopescu1394@gmail.com>
2021-12-01 23:34:01 +0200

    Fix problem 1 from Advent of Code

---
+++ src/day01/Day01.kt
@@ -0,0 +1,34 @@
+package day01
+
+import readInput
+
+fun main() {
+    fun part1(input: List<Int>): Int {
+        var count = 0
+        input.forEachIndexed { index, i ->
+            if (index > 0 && i > input[index-1]) {
+                count++
+            }
+        }
+        return count
+    }
+
+    fun part2(input: List<Int>): Int {
+        val summedInput = mutableListOf<Int>()
+        for (i in 2 until input.size) {
+            summedInput.add(
+                input[i] + input[i - 1] + input[i - 2]
+            )
+        }
+        return part1(summedInput)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day01/Day01_test")
+    check(part1(testInput.map { it.toInt() }) == 7)
+    check(part2(testInput.map { it.toInt() }) == 5)
+
+    val input = readInput("day01/Day01")
+    println(part1(input.map { it.toInt() }))
+    println(part2(input.map { it.toInt() }))
+}
